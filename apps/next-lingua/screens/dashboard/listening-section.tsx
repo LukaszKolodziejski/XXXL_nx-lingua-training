e98a566384d190663108d30d416daa7009d0b820
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2022-10-20 13:20:52 +0200

    listing section

--- apps/next-lingua/screens/dashboard/listening-section.tsx
+++ apps/next-lingua/screens/dashboard/listening-section.tsx
@@ -1,43 +1,43 @@
 import React, { FC, useEffect, useState } from 'react';
 
 export const ListeningSection = () => {
-  const [textContent, setTextContent] = useState('Lorem ipsum dolor sit');
+  const [textContent, setTextContent] = useState('');
 
-  const speechHandler = () => {
+  const btnSpeechHandler = () => {
     const msg = new SpeechSynthesisUtterance(textContent);
     msg.lang = 'en-US';
     window.speechSynthesis.speak(msg);
   };
 
+  const btnStyle = `w-20 rounded-[12px] border-2 mx-2 ${
+    textContent ? 'border-sky-700 text-black' : 'border-sky-600 text-gray-500'
+  }`;
+
   return (
-    <div className="bg-red-200 w-[550px] px-4 py-3">
-      {/* Listening section */}
+    <div className="bg-blue-300 w-[550px] px-4 py-3">
       <div className="flex justify-center mb-3">
         <button
-          className="w-20 rounded-[12px] border-2 border-sky-700 mx-2"
-          onClick={speechHandler}
+          className={btnStyle}
+          disabled={!textContent}
+          onClick={btnSpeechHandler}
         >
           Speech
         </button>
-        <button className="w-20 rounded-[12px] border-2 border-sky-700 mx-2">
+        <button
+          className={btnStyle}
+          disabled={!textContent}
+          onClick={() => setTextContent('')}
+        >
           Clear
         </button>
       </div>
-      {/* <textarea className="w-full h-48" > */}
       <textarea
         rows={8}
         cols={60}
-        className="ml-5 p-1"
+        className="ml-5 p-1 bg-gray-300"
         onChange={(e) => setTextContent(e.target.value)}
         value={textContent}
       />
-      {/* </textarea> */}
-      {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
-        consequatur aperiam tempore eveniet consectetur ducimus voluptate hic
-        laudantium reiciendis non fugit cupiditate unde quod, quae dicta laborum
-        temporibus porro odit fuga doloribus repellendus. Ut, repudiandae iste.
-        Ducimus, eum consequatur vitae, neque quidem quas, corporis sequi
-        expedita odio voluptatum laboriosam quibusdam. */}
     </div>
   );
 };
